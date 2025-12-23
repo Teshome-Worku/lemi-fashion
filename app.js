e526cb965d9d5f9e1057d3af4c48cc831042aa2b
Teshome-worku <teshomeworku96@gmail.com>
2025-12-23 21:01:24 +0300

    Implement structural updates and optimizations across multiple modules

--- app.js
+++ app.js
@@ -213,7 +213,7 @@ const server = http.createServer((req, res) => {
         let filePath = path.join(__dirname, "public", req.url === "/" ? "index.html" : req.url + ".html");
         fs.readFile(filePath, (err, content) => {
             if (err) {
-                fs.readFile(path.join(__dirname, "pages", "404.html"), (err, data) => {
+                fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
                     res.writeHead(404, { "Content-Type": "text/html" });
                     res.end(data);
                 });
